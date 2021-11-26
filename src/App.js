b3da59c27bba5bd007c056d53c1af485bd997ea1
Porter <crporter171@gmail.com>
2021-11-26 11:51:21 -0500

    Movie Photos and Rows complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Building Netflix Clone.</h1>
+      <Row
+        title="Netflix Originals"
+        fetchURL={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Family Movies" fetchURL={requests.fetchFamilyMovies} />
+      <Row
+        title="Science Fiction Movies"
+        fetchURL={requests.fetchScienceFictionMovies}
+      />
+      <Row title="Fantasy Movies" fetchURL={requests.fetchFantasyMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
     </div>
   );
 }
